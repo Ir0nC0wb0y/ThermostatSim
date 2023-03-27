99f74116ab575655f0f79b22ff5a0ee4abd9f993
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2023-03-26 23:48:25 -0500

    ignore file not ignoring?

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from tstat_weather import weather
 
 
 w = weather()
-env_temp = w.CalcTemp(time.time())
-#env_temp = w.CalcTemp(1671978321)
+#env_temp = w.CalcTemp(time.time())
+env_temp = w.CalcTemp(1671978321)
 print("Calculated Temp: " + str(env_temp))
