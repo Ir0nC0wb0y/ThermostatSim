8dc53c7528464e454721b6c4e207bd2caf91d7e7
Ir0nC0wb0y <51864419+Ir0nC0wb0y@users.noreply.github.com>
2023-04-23 16:26:03 -0500

    added random temperature to house

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import time
 from datetime import datetime
 from tstat_weather import weather
+from tstat_home import home
 
+# Environment
+home_size            = 1800 # [square feet]
+home_avg_ceiling_hgt =    8 # [feet]
+
+h = home(home_size*home_avg_ceiling_hgt,75)
 
 w = weather()
-#env_temp = w.CalcTemp(time.time())
-env_temp = w.CalcTemp(1671978321)
+env_temp = w.CalcTemp(time.time())
+#env_temp = w.CalcTemp(1671978321)
 print("Calculated Temp: " + str(env_temp))
+
+h.update()
---
+++ tstat_home.py
@@ -0,0 +1,29 @@
+import random
+
+air_specific_heat = 1 # [J/gK]
+air_density = 1275 # [g/m3]
+
+print("importing home module")
+
+class home_state:
+    def __init__(self):
+        self.temp = 70
+
+    def updateTemp(self,newTemp):
+        self.temp = newTemp
+
+class home:
+    def __init__(self,air_vol,initial_temp):
+        self.state = home_state()
+        self.air_mass = air_vol
+        print("Air mass: " + str(self.air_mass))
+        self.temp = initial_temp
+    
+    def update(self):
+        self.state.updateTemp(random.randrange(60,86))
+        print("New Temperature: " + str(self.state.temp))
+        
+
+    # Functional needs:
+        # heat in (based on time @ temp)
+        # heat out
